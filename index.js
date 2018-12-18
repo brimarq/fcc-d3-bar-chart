582a233b9c3d3020828e4f8a0918af5c1397acd0
Brian Marquis <rbmarquis.dev@gmail.com>
2018-12-17 23:41:58 -0600

    refactored using margin convention, yAxis tick format, added yAxis label

--- index.js
+++ index.js
@@ -28,34 +28,43 @@ req.onload = function() {
 function drawSvg() {
   
   // Set initial svg dimensions to 16:10 ratio
-  const h = 400;
-  const w = h * 1.6;
+  const w = 800;
+  const h = w / 1.6;
 
+  const margin = {top: 20, right: 10, bottom: 20, left: 10};
+
+  const width = w - margin.left - margin.right;
+  const height = h - margin.top - margin.bottom;
+
+  /** Set width for each bar */
+  const barWidth = width / json.data.length;
 
   /** Set the scales for x and y axes */
   const xScale = d3.scaleTime()
     .domain([new Date(json.from_date), new Date(json.to_date)])
-    .range([0 , w]);
+    .range([0 , width]);
   const yScale = d3.scaleLinear()
     .domain([0, d3.max(json.data, (d) => d[1])])
-    .range([h, 0]); // keeps the plot right-side-up 
+    .range([height, 0]) // keeps the plot right-side-up 
+    ; 
+  
   const xAxis = d3.axisBottom(xScale);
-  const yAxis = d3.axisLeft(yScale);
+  const yAxis = d3.axisLeft(yScale).tickFormat(d3.format("$,"));
 
   /** Create hidden temp svg to capture axis tick sizes, then remove it */
   let xAxisMaxTickH = 0;
   let yAxisMaxTickW = 0;
-  const tempSvg = d3.select("div#container")
+  const tempSvg = d3.select("div#svg-container")
     .append("svg")
-    .attr("width", w)
-    .attr("height", h)
+    .attr("width", width)
+    .attr("height", height)
     .style("visibility", "hidden")
     ;
   
    // temp x-axis 
   tempSvg.append("g")
     .attr("id", "x-axis-temp")
-    .attr("transform", "translate(0," + h + ")")
+    .attr("transform", "translate(0," + height + ")")
     .call(xAxis);
   // temp y-axis
   tempSvg.append("g")
@@ -66,64 +75,32 @@ function drawSvg() {
   d3.select("g#x-axis-temp")
     .selectAll(".tick")
     .each(function(d) {
-      let h = this.getBBox().height;
-      if (h > xAxisMaxTickH) xAxisMaxTickH = h;
+      let xTickH = Math.ceil(this.getBBox().height);
+      if (xTickH > xAxisMaxTickH) xAxisMaxTickH = xTickH;
     });
   
   d3.select("g#y-axis-temp")
     .selectAll(".tick")
     .each(function(d) {
-      let w = this.getBBox().width;
-      if (w > yAxisMaxTickW) yAxisMaxTickW = w;
+      let yTickW = Math.ceil(this.getBBox().width);
+      if (yTickW > yAxisMaxTickW) yAxisMaxTickW = yTickW;
     });
 
   tempSvg.remove();
   
   console.log(yAxisMaxTickW);
   console.log(xAxisMaxTickH);
-
-  /** Set "margins" for viewBox area outside of chart */
-  const marginSize = 0.1;
-  const margin = {
-    top: h * marginSize,
-    right: h * marginSize,
-    bottom: h * marginSize,
-    left: h * marginSize
-  };
-
-  // Set padding for svg element
-  const padding = h * .1;
-
-
-  /** Set viewBox dimensions  */
-  const vbXmin = 0 - yAxisMaxTickW - margin.left;
-  const vbYmin = 0 - margin.top;
-  // Top and left margins are factored in to make up for their neg values. 
-  const vbXmax = w + margin.right + -vbXmin;
-  const vbYmax = h + xAxisMaxTickH + margin.bottom + -vbYmin;
   
-  /** Chart coordinates */
-  const chartXmid = (w / 2) - (yAxisMaxTickW / 2);
-  const chartYmid = h / 2;
-  
-  /** Set width of each bar */
-  const barWidth = w / json.data.length;
-  
-
-  
-
-  
-  /** Create the svg viewBox */ 
-  const svg = d3.select("div#container")
+  const svg = d3.select("main div#svg-container")
+    // .style("padding-bottom", (100 * (h / w) + "%"))
     .append("svg")
-    // .attr("preserveAspectRatio", "xMinYMin meet")
-    .attr("viewBox", vbXmin + " " + vbYmin + " " + vbXmax + " " + vbYmax)
-    // .attr("width", w)
-    // .attr("height", h)
-    .attr("class", "svg-content");
+    .attr("width", width + margin.left + margin.right + yAxisMaxTickW)
+    .attr("height", height + margin.top + margin.bottom + xAxisMaxTickH)
+    // .attr("id", "svg-content")
+    // .attr("viewBox", "0 0 " + (width + margin.left + margin.right) + " " + (height + margin.top + margin.bottom))
+    ;
 
-
-  /** Create defs */
+  /** Create svg defs */
   const defs = svg.append("defs");
 
   // linearGradients for bars
@@ -144,7 +121,7 @@ function drawSvg() {
         .attr("stop-opacity", "1")
     });
 
-    defs.append("linearGradient")
+  defs.append("linearGradient")
     .attr("id", "grad2")
     .attr("x1", "0%")
     .attr("y1", "0%")
@@ -160,16 +137,31 @@ function drawSvg() {
         .attr("stop-color", "hsl(120, 100%, 90%)")
         .attr("stop-opacity", "1")
     });
+
+  const barChart = svg.append("g")
+    .attr("id", "bar-chart")
+    .attr("transform", "translate(" + (margin.left + yAxisMaxTickW) + "," + margin.top + ")");
   
+  // Temp outline for bar chart
+  barChart.append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", width)
+    .attr("height", height)
+    .attr("stroke", "blue")
+    .attr("stroke-weight", "2")
+    .attr("fill", "none");
+
   /** Chart title text */
-  svg.append("text")
-    .attr("x", chartXmid)
+  barChart.append("text")
+    .attr("x", (width / 2) - (yAxisMaxTickW / 2))
+    .attr("y", 12)
     .style("text-anchor", "middle")
     .style("font-weight", "bold")
-    .text("United States GDP");
+    .text("United States Gross Domestic Product (1947 Q1 - 2015 Q3)");
 
-  // Create bars from json data
-  svg.selectAll("rect")
+  /** Create bars from json data */
+  barChart.selectAll("rect")
     .data(json.data)
     .enter()
     .append("rect")
@@ -177,23 +169,35 @@ function drawSvg() {
     .attr("x", (d, i) => (i * barWidth))
     .attr("y", (d) => yScale(d[1]))
     .attr("width", barWidth)
-    .attr("height", (d) => h - yScale(d[1]))
+    .attr("height", (d) => height - yScale(d[1]))
     .attr("data-date", (d) => d[0])
     .attr("data-gdp", (d) => d[1])
     .attr("fill", "url(#grad1)");
 
   // x-axis 
-  svg.append("g")
+  barChart.append("g")
     .attr("id", "x-axis")
-    .attr("transform", "translate(0," + h + ")")
+    .attr("transform", "translate(0," + height + ")")
     .call(xAxis);
   // y-axis
-  svg.append("g")
+  barChart.append("g")
     .attr("id", "y-axis")
     .attr("transform", "translate(0, 0)")
     .call(yAxis);
+  // y-axis label
+  barChart.append("text")
+    .attr("x", 25)
+    .attr("y", height / 2 )
+    .style("text-anchor", "middle")
+    .text("GDP in Billions")
+    .attr("transform", function(d) { 
+      const x = d3.select(this).attr("x");
+      const y = d3.select(this).attr("y");
+      return "rotate(-90 " + x + " " + y + ")"
+    });
 
-  svg.selectAll(".bar")
+  /** Hover effects for bar fill and tooltip */
+  barChart.selectAll(".bar")
     .on("mouseover", function(d) {
       d3.select(this).attr("fill", "url(#grad2)");
       tooltip
@@ -219,17 +223,9 @@ function drawSvg() {
         });
     })
     .on("mousemove", function(d) { 
-      const year = d[0].slice(0,4);
       tooltip
         .style("top", (d3.event.pageY - 50) + "px")
         .style("left", (d3.event.pageX + 20) + "px");
-        // .style("left", function() {
-        //   if (+year < 1982) {
-        //     return (d3.event.pageX+10) + "px";
-        //   } else {
-        //     return (d3.event.pageX-150) + "px";
-        //   }
-        // });
     })
     .on("mouseout", function() {
       d3.select(this).attr("fill", "url(#grad1)");
